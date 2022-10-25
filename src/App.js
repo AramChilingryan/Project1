8687de68ac3b4a31fef961d51e7426bd23e30c6f
AramChilingaryan <aramchilngarian@gmail.com>
2022-10-26 00:38:25 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "./form.style.scss"
+import {Formik} from "formik"
+import * as yup from "yup"
+
+const mySchema = yup.object().shape({
+  Email: yup.string().required("Username is requires!"),
+  Password: yup.string().min(8, "login must be at least 8 chars long!")
+})
 
 function App() {
+
+  function submitForm(values,action){
+    console.log(values)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Formik 
+      initialValues={{
+      Email: "",
+      Password: "",
+      }}
+      onSubmit = {submitForm}
+      validationSchema = {mySchema}
+
+    >
+      {
+        ({values,errors,handleSubmit,handleChange})=> {
+          return (
+            <div className='container'>
+  
+              <form autoComplete='off' onSubmit={handleSubmit}>
+                <label htmlFor = "email">Username</label>
+                <input type = "email" name = "Email" id ='email' onChange={handleChange} value = {values.Email}/>
+                <p>{errors.Email}</p>
+                <label htmlFor = "password">Password</label>
+                <input type = "password" name = "Password" id= "password" onChange={handleChange} value = {values.Password}/>
+                <p>{errors.Password}</p>
+                <input type = "submit" value = "Submit" />
+              </form>
+       
+            </div>
+          )
+        }
+      }
+    </Formik>
+  
+  )
 }
 
-export default App;
+export default App
