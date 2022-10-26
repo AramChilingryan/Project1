b14f21ca4c05c7bac075cc49082e722c57915e30
AramChilingaryan <aramchilngarian@gmail.com>
2022-10-27 00:37:08 +0400

    router

--- src/App.js
+++ src/App.js
@@ -1,50 +1,22 @@
 import React from 'react'
-import "./styles/form.style.scss"
-import {Formik} from "formik"
-import * as yup from "yup"
-
-const mySchema = yup.object().shape({
-  Email: yup.string().required("Username is requires!"),
-  Password: yup.string().min(8, "login must be at least 8 chars long!")
-})
+import LoginComponent from "./components/LoginComponent"
+import Dashboard from './components/Dashboard'
+import {Routes, Route,Link} from "react-router-dom"
 
 function App() {
-
-  function submitForm(values,action){
-    console.log(values)
-  }
-
   return (
-    <Formik 
-      initialValues={{
-      Email: "",
-      Password: "",
-      }}
-      onSubmit = {submitForm}
-      validationSchema = {mySchema}
+    <div>
+        <nav>
+             <Link to= "/">Home </Link>
+             <Link to = "/login">Login </Link>
+        </nav>
+        <Routes>
+            <Route  path='/'/>
+            <Route path='/login' element = { <LoginComponent />}/>
+            <Route path='/dashboard' element ={<Dashboard />}/>
+        </Routes>
 
-    >
-      {
-        ({values,errors,handleSubmit,handleChange})=> {
-          return (
-            <div className='container'>
-  
-              <form autoComplete='off' onSubmit={handleSubmit}>
-                <label htmlFor = "email">Username</label>
-                <input type = "email" name = "Email" id ='email' onChange={handleChange} value = {values.Email}/>
-                <p>{errors.Email}</p>
-                <label htmlFor = "password">Password</label>
-                <input type = "password" name = "Password" id= "password" onChange={handleChange} value = {values.Password}/>
-                <p>{errors.Password}</p>
-                <input type = "submit" value = "Submit" />
-              </form>
-       
-            </div>
-          )
-        }
-      }
-    </Formik>
-  
+    </div>
   )
 }
 
---
+++ src/components/LoginComponent.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import "../styles/form.style.scss"
+import {Formik} from "formik"
+import * as yup from "yup"
+import { useNavigate } from 'react-router-dom'
+
+const mySchema = yup.object().shape({
+  Email: yup.string().required("Username is requires!"),
+  Password: yup.string().min(8, "login must be at least 8 chars long!")
+})
+
+function LoginComponent() {
+  const navigate = useNavigate()
+
+  function submitForm(values,action){
+    console.log(values)
+    navigate("/dashboard")
+  }
+
+  return (
+    <Formik 
+      initialValues={{
+      Email: "",
+      Password: "",
+      }}
+      onSubmit = {submitForm}
+      validationSchema = {mySchema}
+
+    >
+      {
+        ({values,errors,handleSubmit,handleChange})=> {
+          return (
+            <div className='container'>
+  
+              <form autoComplete='off' onSubmit={handleSubmit}>
+                <label htmlFor = "email">Username</label>
+                <input type = "email" name = "Email" id ='email' onChange={handleChange} value = {values.Email}/>
+                <p>{errors.Email}</p>
+                <label htmlFor = "password">Password</label>
+                <input type = "password" name = "Password" id= "password" onChange={handleChange} value = {values.Password}/>
+                <p>{errors.Password}</p>
+                <input type = "submit" value = "Submit" />
+              </form>
+       
+            </div>
+          )
+        }
+      }
+    </Formik>
+  
+  )
+}
+
+export default LoginComponent
